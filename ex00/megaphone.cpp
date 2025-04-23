2497ba9f096e2a15aa382892e25ea3cb3b736eeb
mgaudin <matthieugaudin917@gmail.com>
2025-04-23 18:29:06 +0200

    CPP Module 00 exo 00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,31 +1,16 @@
 #include <iostream>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
-	int	i;
-	int	j;
-
-	if (argc == 1)
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		i = 1;
-		while (argv[i])
-		{
-			j = 0;
-			while (argv[i][j])
-			{
-				if (('A' <= argv[i][j] && argv[i][j] <= 'Z')
-					|| ('a' <= argv[i][j] && argv[i][j] <= 'z'))
-					std::cout << argv[i][j] -32; // to convert
-				j++;
+	} else {
+		for (int i = 1; argv[i]; i++) {
+			for (int j = 0; argv[i][j]; j++) {
+				std::cout << (unsigned char)std::toupper(argv[i][j]);
 			}
-			i++;
 		}
 		std::cout << std::endl;
 	}
 }
-
-// what function can i use form std ? (see discord)
-// understand for loop
-// 

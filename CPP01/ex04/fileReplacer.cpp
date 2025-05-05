c56602565748228edae6c8680357a821d6949e0c
mgaudin <mgaudin@student.42angouleme.fr>
2025-05-05 18:35:23 +0200

    fix: the document as a source is correctly read

--- CPP01/ex04/fileReplacer.cpp
+++ CPP01/ex04/fileReplacer.cpp
@@ -16,11 +16,17 @@ fileReplacer::~fileReplacer(void)
 
 std::string	fileReplacer::_readSrcFilename(void) const
 {
-	std::string	input;
-
-	std::ifstream	src(this->_srcFilename.c_str());
-	src >> input;
-	src.close();
+	std::string		input;
+	std::string		buffer;
+	std::ifstream	srcFile;
+	
+	// add flags
+	srcFile.open(this->_srcFilename.c_str());
+	while (std::getline(srcFile, buffer))
+	{
+		input += buffer += "\n";
+	}
+	srcFile.close();
 
 	return (input);
 }
@@ -34,7 +40,7 @@ std::string	fileReplacer::_replaceContent(std::string &input)
 	while (i < input.length())
 	{
 		if (input.substr(i, this->_search.length()).compare(this->_search) == 0) {
-			output.append("PPPPPP");
+			output.append(input.substr(i, this->_search.length()));
 			i += this->_search.length();
 		} else {
 			output.append(1, input[i]);

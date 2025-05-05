16a4171c14a929c723cec064b3274a4490cdb588
mgaudin <mgaudin@student.42angouleme.fr>
2025-05-05 19:08:57 +0200

    CPP[01][04]: error of a file without permissions fixed

--- CPP01/ex04/fileReplacer.cpp
+++ CPP01/ex04/fileReplacer.cpp
@@ -20,7 +20,12 @@ std::string	fileReplacer::_readSrcFilename(void) const
 	std::string		buffer;
 	std::ifstream	srcFile;
 
-	srcFile.open(this->_srcFilename.c_str());
+	srcFile.open(this->_srcFilename.c_str(), std::ios::in);
+	if (!srcFile.is_open())
+	{
+		std::cerr << "replacer (error): file do not have correct rights" << std::endl;
+		exit (0);
+	}
 	while (std::getline(srcFile, buffer))
 	{
 		input += buffer += "\n";
@@ -40,7 +45,6 @@ std::string	fileReplacer::_replaceContent(std::string &input)
 		std::cerr << "replacer (error): can't replace nothing" << std::endl;
 		exit(0);
 	}
-
 	i = 0;
 	while (i < input.length())
 	{
@@ -57,10 +61,10 @@ std::string	fileReplacer::_replaceContent(std::string &input)
 
 void	fileReplacer::_writeDstFilename(std::string &output)
 {
-	this->_dstFilename = this->_srcFilename + ".replace";
 	std::ofstream	dstFile;
 
-	dstFile.open(this->_dstFilename.c_str());
+	this->_dstFilename = this->_srcFilename + ".replace";
+	dstFile.open(this->_dstFilename.c_str(), std::ios::out | std::ios::trunc);
 	dstFile << output;
 
 	dstFile.close();

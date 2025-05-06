27865420d3ff260691656498388c918bbb287f8c
mgaudin <mgaudin@student.42angouleme.fr>
2025-05-06 10:33:04 +0200

    CPP[01][05]: exercice done

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,51 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	return ;
+}
+
+Harl::~Harl(void)
+{
+	return ;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	return ;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	return ;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	return ;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	return ;
+}
+
+void	Harl::complain(std::string level)
+{
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	size_t levelsLenght = sizeof(levels) / sizeof(levels[0]);
+	void	(Harl::*funcTab[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (size_t i = 0; i < levelsLenght; i++)
+	{
+		if(level == levels[i])
+		{
+			(this->*funcTab[i])();
+		}
+	}
+
+}

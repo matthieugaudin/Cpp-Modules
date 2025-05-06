404a77024998aa7c5ccb705130a0fd29ea3caa7e
mgaudin <mgaudin@student.42angouleme.fr>
2025-05-06 10:46:11 +0200

    CPP[01][05]: add some extra line breaks for readability

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -12,19 +12,22 @@ Harl::~Harl(void)
 
 void	Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. " << std::endl;
+	std::cout << "I really do!" << std::endl << std::endl;
 	return ;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. " << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 	return ;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. " << std::endl;
+	std::cout << "I’ve been coming for years, whereas you started working here just last month." << std::endl << std::endl;
 	return ;
 }
 

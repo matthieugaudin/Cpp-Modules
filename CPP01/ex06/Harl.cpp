ebfe48526e153bababb319b8e0bd90d5b2a90755
mgaudin <mgaudin@student.42angouleme.fr>
2025-05-06 11:56:36 +0200

    CPP[01][06]: use a switch statement to resolve the porblem

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -12,6 +12,7 @@ Harl::~Harl(void)
 
 void	Harl::debug(void)
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. " << std::endl;
 	std::cout << "I really do!" << std::endl << std::endl;
 	return ;
@@ -19,6 +20,7 @@ void	Harl::debug(void)
 
 void	Harl::info(void)
 {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. " << std::endl;
 	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 	return ;
@@ -26,6 +28,7 @@ void	Harl::info(void)
 
 void	Harl::warning(void)
 {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. " << std::endl;
 	std::cout << "I’ve been coming for years, whereas you started working here just last month." << std::endl << std::endl;
 	return ;
@@ -33,34 +36,35 @@ void	Harl::warning(void)
 
 void	Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 	return ;
 }
 
 void	Harl::complain(std::string level)
 {
-	
 	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	size_t levelsLenght = sizeof(levels) / sizeof(levels[0]);
-	void	(Harl::*funcTab[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	size_t 		levelsLenght = sizeof(levels) / sizeof(levels[0]);
+	int 		iLevel = 5;
 
-	bool	levelExist = false;
-	for (size_t i = 0; i < levelsLenght; i++)
-	{
-		if(level == levels[i])
-		{
-			levelExist = true;
-			while (i < levelsLenght)
-			{
-				std::cout << "[ " << levels[i] << " ]" << std::endl;
-				(this->*funcTab[i])();
-				std::cout << std::endl;
-				i++;
-			}
-		}
+	for (size_t i = 0; i < levelsLenght; i++) {
+		if (level == levels[i])	
+			iLevel = i + 1;
 	}
-	if (!levelExist) {
-		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+
+	switch (iLevel)
+	{
+		case 1:
+			this->debug();
+		case 2:
+			this->info();
+		case 3:
+			this->warning();
+		case 4:
+			this->error();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 
 }

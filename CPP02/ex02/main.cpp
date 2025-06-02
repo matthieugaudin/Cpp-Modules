adc143dd9d4fc562ad9be998bae3ea74d30d9209
mgaudin <mgaudin@student.42angouleme.fr>
2025-06-02 11:24:38 +0200

    CPP[02][02]: main of tests covers a lot of cases

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -4,20 +4,60 @@ int main( void )
 {
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
 
-    // std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << "== comparaison" << std::endl;
+    std::cout << Fixed(5) / Fixed(2) << std::endl;
+    std::cout << (Fixed(5) == 5 ? "true" : "false") << std::endl;
+    std::cout << (Fixed(5) == 5.1f ? "true" : "false") << std::endl << std::endl;
+
+    std::cout << "> comparaison" << std::endl;
+    std::cout << (Fixed(5) > Fixed(6) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(6) > Fixed(5) ? "true" : "false") << std::endl << std::endl;
+
+    std::cout << "< comparaison" << std::endl;
+    std::cout << (Fixed(5) < Fixed(6) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(6) < Fixed(5) ? "true" : "false") << std::endl << std::endl;
+
+    std::cout << ">= comparaison" << std::endl;
+    std::cout << (Fixed(5) >= Fixed(6) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(6) >= Fixed(5) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(6) >= Fixed(6) ? "true" : "false") << std::endl << std::endl;
+
+    std::cout << "<= comparaison" << std::endl;
+    std::cout << (Fixed(5) <= Fixed(6) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(6) <= Fixed(5) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(6) <= Fixed(6) ? "true" : "false") << std::endl << std::endl;
+
+    std::cout << "!= comparaison" << std::endl;
+    std::cout << (Fixed(6) != Fixed(5) ? "true" : "false") << std::endl;
+    std::cout << (Fixed(5) != Fixed(5) ? "true" : "false") << std::endl << std::endl;
+
+    std::cout << "+, -, *, / operations" << std::endl;
+    std::cout << Fixed(0.5f) + Fixed(0.375f) << std::endl;
+    std::cout << Fixed(0.5f) - Fixed(0.375f) << std::endl;
+    std::cout << Fixed(0.5f) * Fixed(0.375f) << std::endl;
+    std::cout << Fixed(0.5f) / Fixed(0.375f) << std::endl;
+    std::cout << Fixed(0.5f) / Fixed(0) << std::endl << std::endl;
+
+    std::cout << "increment/decrement operators" << std::endl;
+    std::cout << a << std::endl;
+    ++a;
+    std::cout << a << std::endl;
+    a++;
+    std::cout << a << std::endl;
+    --a;
+    std::cout << a << std::endl;
+    a--;
+    std::cout << a << std::endl << std::endl;
+
+
+    std::cout << "min/max methods" << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << Fixed::max(Fixed(11), b) << std::endl;
+    std::cout << Fixed::min( a, b ) << std::endl;
+    std::cout << Fixed::min(Fixed(11), b) << std::endl;
 
     return 0;
 }
 
-// TO DO
-// 4 member functions
-// protect 0 division !
 // make a good main of tests
-// add all const

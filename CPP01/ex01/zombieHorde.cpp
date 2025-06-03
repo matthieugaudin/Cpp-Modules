232877f6a76c1a5b04a25b2d660ce70863edce58
Matthieu Gaudin <mgaudin@1A3.42angouleme.fr>
2025-06-03 15:49:25 +0200

    CPP[01][01]: protect negative nb of zombie allocated

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -4,6 +4,8 @@ Zombie*	zombieHorde(int N, std::string name) {
 
 	Zombie	*horde;
 
+	if (N <= 0)
+		return (NULL);
 	horde = new Zombie[N];
 	for (int i = 0; i < N; i++) {
 		horde[i].setName(name);

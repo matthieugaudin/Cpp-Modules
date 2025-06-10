7e127d9138b3990a8b4f03168bccf9eaddd04d94
mgaudin <matthieugaudin917@gmail.com>
2025-06-10 12:06:15 +0200

    CPP[00][00]: use iterator and std::string to go through argument vector

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <iterator>
 
 int main(int argc, char **argv) {
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	} else {
-		for (int i = 1; argv[i]; i++) {
-			for (int j = 0; argv[i][j]; j++) {
-				std::cout << static_cast<unsigned char>(std::toupper(argv[i][j]));
+		for (size_t i = 1; i < static_cast<size_t>(argc); ++i) {
+			const std::string arg = argv[i];
+			for (std::string::const_iterator it = arg.begin(); it != arg.end(); ++it) {
+				std::cout << static_cast<unsigned char>(std::toupper(*it));
 			}
 		}
 		std::cout << std::endl;

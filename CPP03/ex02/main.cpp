597f3769d7dbdebb4f33ac0b3133a39503566e18
Matthieu Gaudin <mgaudin@1A7.42angouleme.fr>
2025-06-10 21:20:51 +0200

    CPP[03][02]: add required files to the exercice

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -1,10 +1,23 @@
 #include "FragTrap.hpp"
+#include "ScavTrap.hpp"
 #include "ClapTrap.hpp"
 
 int	main(void)
 {
     FragTrap fragBob("Fragbob");
     FragTrap ok = fragBob;
+    ScavTrap scavBob("Scavbob");
+    ScavTrap okBob = scavBob;
+
+    okBob.attack("bob");
+    scavBob.attack("Someone");
+    scavBob.attack("Someone");
+    scavBob.beRepaired(10);
+    scavBob.takeDamage(50);
+    scavBob.takeDamage(65);
+    scavBob.attack("Someone");
+    scavBob.beRepaired(10);
+    scavBob.guardGate();
 
     ok.attack("bob");
     fragBob.attack("Someone");

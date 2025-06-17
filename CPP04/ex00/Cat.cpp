eef16abf49fc0c2479d579d80fd3a91ff4792240
Matthieu Gaudin <mgaudin@1B4.42angouleme.fr>
2025-06-17 14:25:19 +0200

    CPP[04][00]: implement Animal, Dog and Cat classes

---
+++ CPP04/ex00/Cat.cpp
@@ -0,0 +1,30 @@
+#include "Cat.hpp"
+
+Cat::Cat(void)
+{
+	std::cout << "Cat constructor called" << std::endl;
+    this->_type = "Cat";
+}
+
+Cat::Cat(const Cat &src)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+    this->_type = "Cat";
+	*this = src;
+}
+
+Cat::~Cat(void)
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &rhs)
+{
+	Animal::operator=(rhs);
+    return (*this);
+}
+
+void    Cat::makeSound(void) const
+{
+	std::cout << "Miaou Miaou" << std::endl;
+}
